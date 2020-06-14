6abffe5cea603a61e4bb48635bbade34bada6a2b
Egon Willighagen <egon.willighagen@gmail.com>
2020-06-14 18:00:08 +0200

    Omit the URL fields

--- references.js
+++ references.js
@@ -4,6 +4,7 @@ const Cite = require('citation-js');
 
 fs.readFile('references.qids', 'utf8', async function (err, file) {
   const data = Array.from(await Cite.async(file))
+    .map(item => { delete item.URL; return item })
     .map(item => item.id + '=' + Cite(item).format('bibliography', {
       template: 'vancouver',
       append: function (entry) {
